b86a3d059ce2f41dfbe12d08cfbf0c4882af2a3e
fredwilliamson <fabien.messager@gmail.com>
2017-02-19 13:43:49 +0100

    Premier Commit

---
+++ serveurNode.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/etage/:etagenum/chambre', function(req, res) {
+    res.render('chambre.ejs', {etage: req.params.etagenum});
+
+}).get('/compter/:nombre', function(req, res) {
+    var noms = ['Robert', 'Jacques', 'David'];
+    res.render('page.ejs', {compteur: req.params.nombre, noms: noms});
+});
+
+app.listen(8080);
